43afeb8334b5461ecd5f7fe01bfc424399487c65
ElishaShmalo <elisha.shmalo@gmail.com>
2025-07-28 12:46:49 -0400

    made submit job file

---
+++ submit_job.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#SBATCH --partition=main            # Partition (job queue)
+
+#SBATCH --requeue                   # Return job to the queue if preempted
+
+#SBATCH --job-name=bvar_Nvar_etavar         # Assign a short name to your job
+
+#SBATCH --nodes=1                   # Number of nodes you require
+
+#SBATCH --ntasks=1                 # Total # of tasks across all nodes
+
+#SBATCH --cpus-per-task=1           # Cores per task (>1 if multithread tasks)
+
+#SBATCH --mem=256                # Real memory (RAM) required (MB)
+
+#SBATCH --time=00:05:00             # Total run time limit (HH:MM:SS)
+
+#SBATCH --output=slurm.%N.%j.out    # STDOUT output file
+
+#SBATCH --error=slurm.%N.%j.err     # STDERR output file (optional)
+
+
+module load openmpi
+
+
+export OMP_NUM_THREADS=1
+
+
+
+
+srun  --mpi=pmi2  ~/.juliaup/bin/julia julia_code/test.jl
