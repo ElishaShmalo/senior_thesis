541f089518b758c0d6c62d4e80e0819c45c99ef7
ElishaShmalo <elisha.shmalo@gmail.com>
2025-07-30 12:07:51 -0400

    test for N3

--- julia_code/test.jl
+++ julia_code/test.jl
@@ -1,12 +1,5 @@
 include("utils/general.jl")
 
-# Define the full file path
-file_path = "parent1/parent2/test.txt"
+round(Int, 256/3) * 3
 
-# Extract the parent directory
-parent_dir = dirname(file_path)
-
-println(dirname("data/spin_chain_lambdas/data/spin_chain_lambdas/N2/SeveralAs/IC10/L256/N2_ar0p5_0p8_IC10_L256.dat.dat"))
-
-# Ensure the parent directories exist
-mkpath(parent_dir)
+256/3 * 3
