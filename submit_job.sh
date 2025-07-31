bae240b485ee651a16701e30f4765ceb5eb65deb
ElishaShmalo <elisha.shmalo@gmail.com>
2025-07-31 12:03:14 -0400

    first ever parallel task complete

--- submit_job.sh
+++ submit_job.sh
@@ -14,7 +14,7 @@
 
 #SBATCH --mem=150000               # Real memory (RAM) required (MB)
 
-#SBATCH --time=20:00:00             # Total run time limit (HH:MM:SS)
+#SBATCH --time=48:00:00             # Total run time limit (HH:MM:SS)
 
 #SBATCH --output=slurm.%N.%j.out    # STDOUT output file
 
