210872bc4818a380cf26ad9a8d6b8a3680539622
ElishaShmalo <elisha.shmalo@gmail.com>
2025-08-23 20:55:17 -0400

    automate

---
+++ my_git_update.sh
@@ -0,0 +1,28 @@
+git add data/spin_dists_per_time/N4/a0p7575/IC1/L512/
+git commit -m "got lost of data for L512"
+git push
+
+git add data/spin_dists_per_time/N4/a0p76/IC1/L512/
+git commit -m "got lost of data for L512"
+git push
+
+git add data/spin_dists_per_time/N4/a0p7625/IC1/L512/
+git commit -m "got lost of data for L512"
+git push
+
+git add data/spin_dists_per_time/N4/a0p765/IC1/L512/
+git commit -m "got lost of data for L512"
+git push
+
+git add data/spin_dists_per_time/N4/a0p7675/IC1/L512/
+git commit -m "got lost of data for L512"
+git push
+
+git add data/spin_dists_per_time/N4/a0p8/IC1/L512/
+git commit -m "got lost of data for L512"
+git push
+
+git add data/spin_dists_per_time/N4/a0p9/
+git commit -m "got lost of data for L512"
+git push
+
