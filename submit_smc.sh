265379de22b4302e74faecd4628997e56db5178b
ElishaShmalo <elisha.shmalo@gmail.com>
2025-10-08 12:03:56 -0400

    L800 stk

--- submit_smc.sh
+++ submit_smc.sh
@@ -14,7 +14,7 @@
 
 #SBATCH --mem=250000               # Real memory (RAM) required (MB)
 
-#SBATCH --time=24:00:00             # Total run time limit (HH:MM:SS)
+#SBATCH --time=10:00:00             # Total run time limit (HH:MM:SS)
 
 #SBATCH --output=slurm.%N.%j.out    # STDOUT output file
 
