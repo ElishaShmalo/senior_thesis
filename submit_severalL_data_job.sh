f7794af35e0936e8074d7bcdd2bfe07d26e145d8
ElishaShmalo <elisha.shmalo@gmail.com>
2025-10-14 13:57:52 -0400

    getting none random data both

--- submit_severalL_data_job.sh
+++ submit_severalL_data_job.sh
@@ -14,7 +14,7 @@
 
 #SBATCH --mem=250000               # Real memory (RAM) required (MB)
 
-#SBATCH --time=5:00:00             # Total run time limit (HH:MM:SS)
+#SBATCH --time=05:00:00             # Total run time limit (HH:MM:SS)
 
 #SBATCH --output=slurm.%N.%j.out    # STDOUT output file
 
