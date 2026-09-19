e2074b980b359877dddb4872ad0a6adc003c928b
ElishaShmalo <elisha.shmalo@gmail.com>
2026-09-19 14:16:08 -0400

    hunting ep c for upper lower

--- stavskya_mc/get_time_random_uppper_lower_binary_time_data.jl
+++ stavskya_mc/get_time_random_uppper_lower_binary_time_data.jl
@@ -14,9 +14,9 @@ addprocs(SlurmManager())
 
     float_str(f) = replace("$f", "." => "p")
 
-    L_vals = [20000]
+    L_vals = [35000]
     average_epsilon_c =    0.27033
-    average_epsilon_rate = 0.0001
+    average_epsilon_rate = 0.00005
     p_val = 0.8
     lower_div = 20
     # epsilon_bar = (epsilon_u + epsilon_l) / 2 = 0.55 * epsilon_u.
